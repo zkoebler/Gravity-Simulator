a4447d4229226d73d54bf9df803df8789ed7f5b1
Zephrom <zkoebler@gmail.com>
2021-10-12 17:02:24 -0700

    Finally achieved orbit!

--- sketch.js
+++ sketch.js
@@ -4,12 +4,13 @@
 function setup() {
     createCanvas(1000, 1000);
     sunLocation = createVector(500,500);
-    earthLocation = createVector(sunLocation.x/2,sunLocation.y/2);
+    earthLocation = createVector(sunLocation.x-400,sunLocation.y);
+    //frameRate(1);
   }
   
   
   function draw() {
-    background(255);
+    background(30);
     fill(255, 255, 0);
     circle(sunLocation.x,sunLocation.y,100);
     
@@ -23,14 +24,22 @@ function setup() {
     forceGravity = G*(m1*m2)/pow((r).mag(),2);
     rUnit = p5.Vector.normalize(r);
 
-    if(r.mag() > 50){
+    if(r.mag() > 30){
       earthLocation.x += (rUnit.x)*forceGravity;
       earthLocation.y += (rUnit.y)*forceGravity;
     }
     else{
-      earthLocation.x = sunLocation.x;
-      earthLocation.y =sunLocation.y;
+      earthLocation.x = random(0,500);
+      earthLocation.y = random(0,500);
+      return;
     }
-
+    
+    rUnit.rotate(HALF_PI);
+    let initialV = forceGravity*40;
+    earthLocation.x += (rUnit.x)*initialV;
+    earthLocation.y += (rUnit.y)*initialV;
+    
+    
+    
    
   }
